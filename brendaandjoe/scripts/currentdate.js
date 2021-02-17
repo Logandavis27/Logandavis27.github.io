c2073883975035d9870963f03a4c88d3a3ecce13
ldavis27 <logandavis27@gmail.com>
2021-02-16 21:55:30 -0500

    test

--- brendaandjoe/scripts/currentdate.js
+++
@@ -1,9 +0,0 @@
-qday=new Array("Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday");
-qmonth=new Array("January","February","March","April","May","June","July","August","September","October","November","December");
-function getDateAndTime(){
-    var d=new Date();
-    var nday=d.getDay(),nmonth=d.getMonth(),ndate=d.getDate(),nyear=d.getYear();
-    if(nyear<1000) nyear+=1900;
-    document.getElementById("currentdate").innerHTML=""+qday[nday]+", "+ndate+" "+qmonth[nmonth]+", "+nyear+"";
-}
-getDateAndTime();
